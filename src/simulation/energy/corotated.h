ea0a809a573911a619530fc85b6bd17e5c83b5c9
Zhenggehan <31180310+zeblooder@users.noreply.github.com>
2024-08-31 23:46:49 +0800

    simplify corotation's gradient

--- src/simulation/energy/corotated.h
+++ src/simulation/energy/corotated.h
@@ -35,14 +35,19 @@ namespace Corotated {
         svdRV(F, U, S, V);
         glm::tmat3x3<HighP> R = U * glm::transpose(V);
         glm::tmat3x3<HighP> P = 2 * mu[index] * (F - R) + lambda[index] * trace(glm::transpose(R) * F - glm::tmat3x3<HighP>(1)) * R;
-        Matrix9x12<HighP> PFPx = ComputePFPx(DmInv);
-        Vector12<HighP> dPsidx = coef * Vol[index] * PFPx.transpose() * Vector9<HighP>(P);
-        for (int i = 0; i < 3; i++) {
-            atomicAdd(&grad[v0Ind + i], dPsidx[i]);
-            atomicAdd(&grad[v1Ind + i], dPsidx[3 + i]);
-            atomicAdd(&grad[v2Ind + i], dPsidx[6 + i]);
-            atomicAdd(&grad[v3Ind + i], dPsidx[9 + i]);
-        }
+        glm::tmat3x3<HighP> dPsidx = coef * Vol[index] * P * glm::transpose(DmInv);
+        atomicAdd(&grad[v0Ind + 0], -dPsidx[0][0]-dPsidx[1][0]-dPsidx[2][0]);
+        atomicAdd(&grad[v0Ind + 1], -dPsidx[0][1]-dPsidx[1][1]-dPsidx[2][1]);
+        atomicAdd(&grad[v0Ind + 2], -dPsidx[0][2]-dPsidx[1][2]-dPsidx[2][2]);
+        atomicAdd(&grad[v1Ind + 0], dPsidx[0][0]);
+        atomicAdd(&grad[v1Ind + 1], dPsidx[0][1]);
+        atomicAdd(&grad[v1Ind + 2], dPsidx[0][2]);
+        atomicAdd(&grad[v2Ind + 0], dPsidx[1][0]);
+        atomicAdd(&grad[v2Ind + 1], dPsidx[1][1]);
+        atomicAdd(&grad[v2Ind + 2], dPsidx[1][2]);
+        atomicAdd(&grad[v3Ind + 0], dPsidx[2][0]);
+        atomicAdd(&grad[v3Ind + 1], dPsidx[2][1]);
+        atomicAdd(&grad[v3Ind + 2], dPsidx[2][2]);
     }
 
     template <typename HighP>
